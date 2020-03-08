bdcc142f1cc371dd5ab5ecafb2455d93841e57d4
승가리 <imsa1224@korea.ac.kr>
2020-03-08 21:04:30 +0900

    contextapi

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Store from "store";
+import AppPresenter from "./AppPresenter";
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this._deleteNotification = id => {
+      this.setState(currentState => {
+        const newState = delete currentState.notifications[id];
+        return newState;
+      });
+    };
+    this._seeNotification = id => {
+      this.setState(currentState => {
+        return {
+          ...currentState,
+          notifications: {
+            ...currentState.notifications,
+            [id]: {
+              ...currentState.notifications[id],
+              seen: true
+            }
+          }
+        };
+      });
+    };
+    this.state = {
+      notifications: {
+        "1": {
+          id: 1,
+          text: "Something",
+          seen: false
+        },
+        "2": {
+          id: 2,
+          text: "Something else",
+          seen: false
+        },
+        "3": {
+          id: 3,
+          text: "Something dif",
+          seen: false
+        }
+      },
+      deleteNotification: this._deleteNotification,
+      seeNotification: this._seeNotification
+    };
+  }
+
+  render() {
+    return (
+      <Store.Provider value={this.state}>
+        <AppPresenter />
+      </Store.Provider>
+    );
+  }
+}
+
+export default AppContainer;
